1ab67a231e8c3bf3082d011a4a4fb075447d1e9a
josh-wilkinson <91944995+josh-wilkinson@users.noreply.github.com>
2022-01-27 16:04:33 +0000

    no jakes allowed

---
+++ prj/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <link rel="stylesheet" href="styles.css">   <!--This links all my css styles-->
+</head>
+
+<body>    
+    <header>
+        <nav>
+            <li>
+                <ul>
+                    <li class="logo"><a href="">Website Logo</a></li>
+                    <li><a href="">option 1</a>
+                        <ul><li><a href="">1</a></li></ul>
+                    </li>
+                    <li><a href="">option 2</a>
+                        <ul><li><a href="">2</a></li></ul>
+                    </li>
+                    <li><a href="">option 3</a>
+                        <ul><li><a href="">3</a></li></ul>
+                    </li>
+                    <li><a href="">option 4</a>
+                        <ul><li><a href="">4</a></li></ul>
+                    </li>
+                </ul>
+            </li>
+        </nav>
+    </header>
+    
+</body>
+
+
+</html>
---
+++ prj/main_page.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Main Page</title>
+        <link rel="stylesheet" href="styles.css">   <!--This links all the css styles-->
+    </head>
+
+    
+
+    <body>
+
+    
+        <?php include 'header.php';?>
+    
+        
+        <h1>Welcome to my home page!</h1>
+        <p>Some text.</p>
+        <p>Some more text.</p>
+
+        <?php include 'footer.php';?>
+    </body>
+
+
+
+</html>
