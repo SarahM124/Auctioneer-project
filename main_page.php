d231895b64cad65f13219008a830045e6b3215e2
josh-wilkinson <91944995+josh-wilkinson@users.noreply.github.com>
2022-02-07 12:52:29 +0000

    Add files via upload

---
+++ main_page.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Main Page</title>
+        <link rel="stylesheet" href="styles.css">   <!--This links all the css styles-->
+    </head>
+
+    <?php include 'header.php';?>
+
+    <body>       
+    
+        
+        
+        <div class="video">
+            VIDEO
+        </div>
+
+        <div class="textbox">
+            <h2>
+                    abcd Welcome to the website!
+            </h2>
+        </div>
+
+        <div class="textbox2">
+            <h2>
+                abcd Welcome to the website!
+            </h2>
+        </div>
+
+        <footer>
+            <?php include 'footer.php';?>
+        </footer>
+    </body>
+
+    
+
+</html>
